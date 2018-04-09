5a4050f3d2984f408332ce671604aa2e78c6e9c4
Marc Tiehuis <marctiehuis@gmail.com>
2018-04-09 22:26:42 +1200

    Fix c binding

--- deflate_c.zig
+++ deflate_c.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const hash = std.hash;
 const deflate = @import("deflate.zig");
 
 // max decompress is 4096
@@ -8,7 +9,7 @@ export fn zig_decompress(in: &const u8, inlen: usize, out: &&const u8, outlen: &
     // use a fixed buffer allocator to avoid allocating in the loop
     var fixed_allocator = std.heap.FixedBufferAllocator.init(c_output[0..]);
 
-    var buf = deflate.decompress(&fixed_allocator.allocator, in[0..inlen]) catch |err| return -1;
+    var buf = deflate.decompressAlloc(&fixed_allocator.allocator, in[0..inlen], hash.Adler32) catch |err| return -1;
     defer buf.deinit();
 
     // Array is backed by global c_output array so just use directly
